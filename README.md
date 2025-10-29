71ef3ee18c9d40c7ecb04e38240c5c64ccead0d5
PE8RI <ritwickraj5632@gmail.com>
2025-10-29 19:34:59 +0530

    Setup project structure and database connection

---
+++ README.md
@@ -0,0 +1,29 @@
+🌍 WanderLust
+
+WanderLust is a web application that allows users to explore and manage travel listings. It’s built using the MERN stack (currently MongoDB, Express, Node.js) with EJS templates for the frontend.
+
+🚀 Features 
+
+Connected MongoDB database using Mongoose
+
+Defined Listing Schema with default image and validation
+
+Implemented basic CRUD operations
+
+View all listings
+
+View individual listing details
+
+Integrated EJS templating for dynamic pages
+
+Organized project structure with models/, views/, and public/ folders
+
+🛠️ Tech Stack
+
+Frontend: EJS, HTML, CSS
+
+Backend: Node.js, Express.js
+
+Database: MongoDB with Mongoose
+
+Template Engine: EJS
---
+++ app.js
@@ -0,0 +1,77 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Listing=require("./models/listing.js");
+const path=require("path");
+const methodOverride = require("method-override");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+main().then((res)=>{
+  console.log("Connection established");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+app.get("/",(req,res)=>{
+  res.send("working");
+})
+
+//index route
+app.get("/listings",async (req,res)=>{
+  let allListings=await Listing.find({});
+  res.render("listing/index.ejs",{allListings});
+})
+
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listing/new.ejs");
+});
+
+//show route
+app.get("/listings/:id",async (req,res)=>{
+  let {id}=req.params;
+  let listing=await Listing.findById(id);
+  res.render("listing/show.ejs",{listing});
+})
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listing/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listing/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing }, { new: true, runValidators: true });
+
+  res.redirect(`/listings/${id}`);
+});
+
+// //Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+
+app.listen(8080,(req,res)=>{
+  console.log("Server started")
+})
